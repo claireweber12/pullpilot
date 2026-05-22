a56bfb0b26f55eaf2cba864c359c6d14e8a88352
claireweber12 <claire.weber2004@icloud.com>
2026-05-22 11:12:55 -0500

    update headers

--- server/services/githubService.js
+++ server/services/githubService.js
@@ -1,12 +1,15 @@
 export async function getPullRequest(owner, repo, pullNumber){
     const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${pullNumber}`
 
-    const response = await fetch(url, {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        'User-Agent': 'PullPilot',
-      },
-    })
+    const headers = {
+      Accept: 'application/vnd.github+json',
+      'User-Agent': 'PullPilot',
+    }
+
+  if (process.env.GITHUB_TOKEN) {
+    headers.Authorization = `Bearer ${process.env.GITHUB_TOKEN}`
+  } 
+  const response = await fetch(url, {headers})
 
     if(!response.ok){
       const errorText = await response.text()
@@ -20,12 +23,15 @@ export async function getPullRequest(owner, repo, pullNumber){
 export async function getPullRequestFiles(owner, repo, pullNumber) {
   const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${pullNumber}/files`
 
-  const response = await fetch(url, {
-    headers: {
-      Accept: 'application/vnd.github+json',
-      'User-Agent': 'PullPilot',
-    },
-  })
+  const headers = {
+  Accept: 'application/vnd.github+json',
+  'User-Agent': 'PullPilot',
+  }
+
+  if (process.env.GITHUB_TOKEN) {
+  headers.Authorization = `Bearer ${process.env.GITHUB_TOKEN}`
+  }
+  const response = await fetch(url, {headers})
 
   if (!response.ok) {
     const errorText = await response.text()
